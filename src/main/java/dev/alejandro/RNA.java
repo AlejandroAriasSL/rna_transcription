d52b8f8e8652e7eb375db3367a5132e72e92ffab
AlejandroAriasSL <alejandroasilva02@gmail>
2025-03-10 23:41:56 +0100

    test_throws_exception_if_RNA_sequence_invalid() (tdd:green)

--- src/main/java/dev/alejandro/RNA.java
+++ src/main/java/dev/alejandro/RNA.java
@@ -5,7 +5,14 @@ public class RNA {
     private String rnaSequence;
 
     public RNA(String rnaSequence) {
+        validateSequence(rnaSequence);
         this.rnaSequence = rnaSequence;
+    }
+
+    private void validateSequence (String rnaSequence) {
+        if (!rnaSequence.matches("[CGAU]+")) {
+            throw new IllegalArgumentException("Invalid RNA sequence");
+        }
     }
 
     public String getSequence() {
