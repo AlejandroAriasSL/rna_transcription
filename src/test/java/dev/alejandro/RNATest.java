66f0ed01a6259bfa1fa3b16158267dbe75102793
AlejandroAriasSL <alejandroasilva02@gmail>
2025-03-10 23:39:29 +0100

    add test_throws_exception_if_RNA_sequence_invalid() (tdd:red)

--- src/test/java/dev/alejandro/RNATest.java
+++ src/test/java/dev/alejandro/RNATest.java
@@ -2,13 +2,14 @@ package dev.alejandro;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class RNATest {
     
-     @Test
+    @Test
     @DisplayName("It shold return the provided RNA sequence")
     void test_returns_provided_RNA_sequence() {
 
@@ -17,5 +18,18 @@ public class RNATest {
         RNA rna = new RNA(rnaSequence);
 
         assertThat(rna.getSequence(), is(rnaSequence));
+    }
+
+    @Test
+    @DisplayName("It should throw an exception if the provided RNA sequence is not valid")
+    void test_throws_exception_if_RNA_sequence_invalid() {
+
+        String dnaSequence = "GTAR";
+
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new RNA(dnaSequence));
+
+
+        assertThat(exception.getMessage(), is("Invalid RNA sequence"));
     }
 }
