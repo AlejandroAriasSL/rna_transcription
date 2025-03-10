a4083557587515fcf8781a76c332436521d47104
AlejandroAriasSL <alejandroasilva02@gmail>
2025-03-10 23:22:43 +0100

    add test_throws_exception_if_DNA_sequence_invalid() (tdd:red)

--- src/test/java/dev/alejandro/DNATest.java
+++ src/test/java/dev/alejandro/DNATest.java
@@ -1,5 +1,7 @@
 package dev.alejandro;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -16,6 +18,19 @@ public class DNATest {
         DNA dna = new DNA(dnaSequence);
 
         assertThat(dna.getSequence(), is(dnaSequence));
+    }
+
+    @Test
+    @DisplayName("It should throw an error if the provided DNA sequence is not valid")
+    void test_throws_exception_if_DNA_sequence_invalid() {
+
+        String dnaSequence = "GTAR";
+
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new DNA(dnaSequence));
+
+
+        assertThat(exception.getMessage(), is("Invalid DNA sequence"));
     }
     
 }
