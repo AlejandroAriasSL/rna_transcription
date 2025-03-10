95e6ccb019e32d9ae6513e132199c14cf0534523
AlejandroAriasSL <alejandroasilva02@gmail>
2025-03-10 23:23:17 +0100

    test_throws_exception_if_DNA_sequence_invalid() (tdd:green)

--- src/main/java/dev/alejandro/DNA.java
+++ src/main/java/dev/alejandro/DNA.java
@@ -5,7 +5,15 @@ public class DNA {
     private String dnaSequence;
 
     public DNA(String dnaSequence) {
+        validateSequence(dnaSequence);
         this.dnaSequence = dnaSequence;
+    }
+
+    private void validateSequence(String dnaSequence) {
+        
+        if (!dnaSequence.matches("[GCTA]+")) {
+            throw new IllegalArgumentException("Invalid DNA sequence");
+        }
     }
 
     public String getSequence() {
