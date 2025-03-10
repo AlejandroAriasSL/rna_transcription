294d039b13d9260d445cd93e049c2db7b6ffeecd
AlejandroAriasSL <alejandroasilva02@gmail>
2025-03-10 23:37:14 +0100

    test_returns_provided_RNA_sequence() (tdd:green)

---
+++ src/main/java/dev/alejandro/RNA.java
@@ -0,0 +1,14 @@
+package dev.alejandro;
+
+public class RNA {
+    
+    private String rnaSequence;
+
+    public RNA(String rnaSequence) {
+        this.rnaSequence = rnaSequence;
+    }
+
+    public String getSequence() {
+        return rnaSequence;
+    }
+}
