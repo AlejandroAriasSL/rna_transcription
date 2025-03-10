0acc9852afd931b13a7672d22dd65ddd57e8f741
AlejandroAriasSL <alejandroasilva02@gmail>
2025-03-10 22:44:26 +0100

    test_returns_provided_DNA_sequence() (tdd:green)

---
+++ src/main/java/dev/alejandro/DNA.java
@@ -0,0 +1,14 @@
+package dev.alejandro;
+
+public class DNA {
+
+    private String dnaSequence;
+
+    public DNA(String dnaSequence) {
+        this.dnaSequence = dnaSequence;
+    }
+
+    public String getSequence() {
+        return dnaSequence;
+    }
+}
